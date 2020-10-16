647215bb379666c6480f5e8bf30053e10c7cdac1
nanaomg <49023120+nanaomg@users.noreply.github.com>
2020-10-16 14:35:44 +1000

    Update layers.js
    renamed layers?

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,7 @@
 addLayer("c", {
         layer: "W", // This is assigned automatically, both to the layer and all upgrades, etc. Shown here so you know about it
-        name: "Candies", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
+        name: "Web", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         startData() { return {
             unlocked: true,
