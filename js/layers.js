f5c3c9dcdfa072ec23e14ada91a4b4898868cccb
nanaomg <49023120+nanaomg@users.noreply.github.com>
2020-10-16 14:51:09 +1000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,7 @@
-addLayer("W", {
-        layer: "W", // This is assigned automatically, both to the layer and all upgrades, etc. Shown here so you know about it
-        name: "Web", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("c", {
+        layer: "c", // This is assigned automatically, both to the layer and all upgrades, etc. Shown here so you know about it
+        name: "Candies", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         startData() { return {
             unlocked: true,
@@ -11,10 +11,10 @@ addLayer("W", {
             buyables: {}, // You don't actually have to initialize this one
             beep: false,
         }},
-        color: "#C0C0C0",
+        color: "#4BDC13",
         requires: new Decimal(10), // Can be a function that takes requirement increases into account
-        resource: "string", // Name of prestige currency
-        baseResource: "thread", // Name of resource prestige is based on
+        resource: "lollipops", // Name of prestige currency
+        baseResource: "candies", // Name of resource prestige is based on
         baseAmount() {return player.points}, // Get the current amount of baseResource
         type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
         exponent: 0.5, // Prestige currency exponent
