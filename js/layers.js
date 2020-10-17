358228c4ffca4d3bec4c9a91416ce72bf1d16cfa
nanaomg <49023120+nanaomg@users.noreply.github.com>
2020-10-17 23:55:36 +1000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -90,7 +90,6 @@ addLayer("c", {
                 unlocked() { return (hasUpgrade(this.layer, 12))},
                 onPurchase() { // This function triggers when the upgrade is purchased
                     player[this.layer].unlockOrder = 0
-                    return (hasUpgrade(this.layer, 13)),
                 },
             },
             22: {
